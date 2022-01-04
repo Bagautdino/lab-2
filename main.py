48be2d25510d1128ac6c66705badd22ed2b84262
Bagautdino <51779102+Bagautdino@users.noreply.github.com>
2022-01-05 02:17:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,137 +1,113 @@
-import csv
-import requests
-from bs4 import BeautifulSoup
-
-p = ' Пожалуйста, пишите на английском!'
-e = ' Если данный параметр вам не важен, ведите Enter.'
-dialog = {
-    'greeting': ['Здравствуйте! Давайте подберем вам анимэ!'],
-    'questions': ('Выберите жанр?' + p + e,
-                  'Какая минимальная оценка?' + e,
-                  'Количество отзывов больше чем...' + e,
-                  'Чего не должно быть' + p + e,
-                  'Формат' + p + e,
-                  'Сколько минимум эпизодов?' + e,
-                  'Должно быть закончено?' + e,
-                  'От какого года?' + e,
-                  'До какого года?' + e,
-                  'Какой сезон?' + p + e,
-                  'Студия?' + p + e
-                  )
-}
-question = (
-    'Tags',
-    'Rating Score',
-    'Number Votes',
-    'Content Warning',
-    'Type',
-    'Episodes',
-    'Finished',
-    'StartYear',
-    'EndYear',
-    'Season',
-    'Studios'
-)
-answer_equal = {
-    'Type': '',
-    'Finished': '',
-    'StartYear': '',
-    'EndYear': ''
-}
-answer_more = {
-    'Rating Score': '',
-    'Number Votes': '',
-    'Episodes': ''
-}
-answer_in = {
-    'Tags': '',
-    'Season': '',
-    'Studios': ''
-}
-answer_not_in = {
-    'Content Warning': ''
-}
-answers = [answer_in, answer_not_in, answer_more, answer_equal]
+import pandas as pd
+import requests as req
 
 
-def save_answer(ans, q_type, answers):
-    for answer_gr in answers:
-        if q_type in answer_gr:
-            answer_gr[q_type] = ans
-
-#
-def run_dialog(answers, dialog):
-    print(*dialog['greeting'])
-    questions = dialog['questions']
-    for k in range(len(questions)):
-        ans = input(questions[k])
-        save_answer(ans, question[k], answers)
+def find_anime_by_name(df):
+    name = input(str('Напишите название анимэ?\nЕсли это не конкретное анимэ ENTER: '))
+    if name != '':
+        for anime_name in range(len(df['Name'])):
+            if (df['Name'][anime_name]).lower() == name.lower() or \
+                    df['Alternative Name'][anime_name].lower() == name.lower():
+                print(df['Name'][anime_name] + '\n' + df['Url'][anime_name])
+                quit()
+        print('Такого аниме не найдено')
+        quit()
 
 
-def is_equal(answer_in, entry):
-    return True if answer_in in ('', entry) else False
+def find_anime_by_tag(df, collection_anime):
+    tags = str(input('Какой жанр Вас интересует?\n(Введите через запятую, если это не очень важно нажмите ENTER): '))
+    if tags != '':
+        tags_array = tags.split(',')
+        for tag in tags_array:
+            for tag_count in range(len(df['Tags'])):
+                if tag.lower() in df['Tags'][tag_count].lower():
+                    collection_anime.append(df.loc[tag_count, :])
+        if len(collection_anime) == 0:
+            print('Таких жанров не найдено')
+            quit()
 
 
-def is_more(answer_in, entry):
-    if answer_in == '':
-        answer_in = float(0)
-    if entry == 'Unknown':
-        entry = float(0)
-    return True if float(answer_in) <= float(entry) else False
+def find_anime_by_episodes(df, collection_anime):
+    episodes = str(input('Вас интересует короткометражное или полнометражное аниме?\n(ENTER, если не важно): '))
+    if episodes != '':
+        tmp_anime = collection_anime[:] if len(collection_anime) else df
+        collection_anime.clear()
+        for anime in tmp_anime:
+            if episodes.lower() == 'короткометражное':
+                if anime['Episodes'] == 'Unknown':
+                    continue
+                if int(anime['Episodes']) > 1:
+                    collection_anime.append(anime)
+            elif episodes.lower() == 'полнометражное':
+                if anime['Episodes'] == '1':
+                    collection_anime.append(anime)
+        if len(collection_anime) < 1:
+            print('Таких аниме не найдено')
+            quit()
 
 
-def is_in(answer_in, entry):
-    return True if answer_in in entry else False
+def find_anime_by_duration(df, collection_anime):
+    duration = str(input('Какая продолжительность Вас интересует (Кол-во часов)?\n(ENTER, если не важно): '))
+    if duration != '':
+        tmp_anime = collection_anime[:] if len(collection_anime) else df
+        collection_anime.clear()
+        for anime in tmp_anime:
+            if anime['Duration'] == 'Unknown':
+                continue
+            if anime['Duration'] == duration:
+                collection_anime.append(anime)
+        if len(collection_anime) < 1:
+            print('Таких аниме не найдено')
+            quit()
 
 
-def is_not_in(answer_in, entry):
-    return False if answer_in in entry else True
+def rate_sorting(collection_anime):
+    rating_data = []
+    for anime in collection_anime:
+        if anime['Rating Score'] == 'Unknown':
+            continue
+        else:
+            rating_data.append(float(anime['Rating Score']))
+    rating_data = list(set(rating_data))
+    rating_data.sort()
+    rating_data.reverse()
+    tmp_anime = collection_anime
+    collection_anime = []
+    for score in rating_data:
+        for anime in tmp_anime:
+            if anime['Rating Score'] == 'Unknown':
+                continue
+            if score == float(anime['Rating Score']):
+                collection_anime.append(anime)
+        for anime in tmp_anime:
+            if anime['Rating Score'] == 'Unknown':
+                collection_anime.append(anime)
+    return collection_anime
 
 
-run_dialog(answers, dialog)
-answer = []
+def main():
+    collection_anime = []
+    df = pd.read_csv('anime.csv')
+    find_anime_by_name(df)
+    find_anime_by_tag(df, collection_anime)
+    find_anime_by_episodes(df, collection_anime)
+    find_anime_by_duration(df, collection_anime)
+    if len(collection_anime) > 0:
+        rate_sorting(collection_anime)
+        df_good = pd.DataFrame(collection_anime)
+        df_good.to_json('out.json')
 
-with open('anime.csv', newline='', encoding='utf-8') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        good = True
-        for key, val in answer_equal.items():
-            good = good and is_equal(val, row[key])
+        for anime in collection_anime[:5]:
+            url = str('https://www.anime-planet.com/images/anime/covers/'
+                      + str(anime['Anime-PlanetID']) + '.jpg?t=1523213250')
+            name = str(anime['Anime-PlanetID'])
+            img = req.get(url)
+            img_opt = open(name + '.jpg', 'wb')
+            img_opt.write(img.content)
+            img_opt.close()
+        return
+    print('Вам подходят все аниме')
 
-        for key, val in answer_more.items():
-            good = good and is_more(val, row[key])
 
-        for key, val in answer_in.items():
-            for j in val.split():
-                good = good and is_in(j, row[key])
-
-        for key, val in answer_not_in.items():
-            for j in val.split():
-                good = good and is_not_in(j, row[key])
-
-        if good:
-            if row['Rating Score'] == 'Unknown':
-                answer.append([float(0), row['Url'], row['Name']])
-            else:
-                answer.append([float(row['Rating Score']), row['Url'], row['Name']])
-answer.sort()
-answer.reverse()
-f = open('answer.txt', 'w', encoding='utf-8')
-for i in range(min(5, len(answer))):
-    response = requests.get(answer[i][1])  # отправляем запрос на страницу. answer[i][1] - ссылка на странцу с постером.
-    soup = BeautifulSoup(response.text,
-                         'html.parser')  # BeautifulSoup позволяет преобразовать сложный HTML-документ в объекты Python
-    img = requests.get("https://www.anime-planet.com/" + soup.find('img', class_='screenshots')['src'])
-    img_file = open(str(i + 1) + '.jpg', 'wb')
-    img_file.write(img.content)
-    img_file.close()
-    f.write(answer[i][2] + ': ' + answer[i][1] + '\n')
-for i in range(min(5, len(answer)), len(answer)):
-    f.write(answer[i][2] + ': ' + answer[i][1] + '\n')
-f.close()
-
-print('---------------------------------------------------------------------------------------------------------------')
-print('На этом все. Подходящие вашим запросам анимэ и ссылки на них находятся в файле answer.txt.\n')
-print('Анимэ отсортированы в порядке убывания их рейтинга.\n')
-print('Для первых 5 анимэ с лучшим рейтингом вы можете посмотреть постеры.\n')
-print('Они находятся в файлах 1-5.jpg соответственно.')
+if __name__ == '__main__':
+    main()
